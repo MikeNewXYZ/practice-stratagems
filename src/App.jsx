f2aaa6f9e88fe98f1506a076019aba00d62600f5
NotBaldrick <Git@NotBaldrick.com>
2024-03-31 17:40:15 +0100

    add game area

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { tinykeys } from "tinykeys";
+import stratagemConfig from "./config/stratagems.json";
 import Header from "./components/Header";
 import GameArea from "./components/GameArea";
 
@@ -10,26 +11,84 @@ const controlMap = {
 	right: "Control+ArrowRight",
 };
 
+const randomStratagem = (stratagems) => {
+	const randomIndex = Math.floor(Math.random() * stratagems.length);
+	const { name, inputCode } = stratagems[randomIndex];
+
+	return {
+		name: name,
+		inputCode: inputCode.map((id) => ({
+			id: id,
+			correct: false,
+		})),
+	};
+};
+
 function App() {
-	const [inputCode, setInputCode] = useState("");
+	const [inputCodeIndex, setInputCodeIndex] = useState(0);
+	const [stratagems, setStratagems] = useState(
+		[...Array(10)].map(() =>
+			randomStratagem([
+				...stratagemConfig.helldiversOne,
+				...stratagemConfig.helldiversTwo,
+			]),
+		),
+	);
+
+	const cycleStratagems = (inputCode) => {
+		let newStratagems = stratagems;
+
+		// Check if user pressed correct key.
+		if (stratagems[0].inputCode[inputCodeIndex].id === inputCode) {
+			// Cycle to next stratagem.
+			if (inputCodeIndex >= stratagems[0].inputCode.length - 1) {
+				newStratagems.shift();
+				setInputCodeIndex(0);
+				setStratagems([
+					...newStratagems,
+					randomStratagem([
+						...stratagemConfig.helldiversOne,
+						...stratagemConfig.helldiversTwo,
+					]),
+				]);
+			} else {
+				// Cycle to next input code on current stratagem.
+				newStratagems[0].inputCode.splice(inputCodeIndex, 1, {
+					id: newStratagems[0].inputCode[inputCodeIndex].id,
+					correct: true,
+				});
+				setInputCodeIndex((prev) => prev + 1);
+				setStratagems(newStratagems);
+			}
+		} else {
+			// Reset back to start if user pressed wrong key.
+			newStratagems[0].inputCode = newStratagems[0].inputCode.map(({ id }) => ({
+				id: id,
+				correct: false,
+			}));
+			setInputCodeIndex(0);
+			setStratagems(newStratagems);
+		}
+	};
 
 	useEffect(() => {
 		const unsubscribe = tinykeys(window, {
-			[controlMap.up]: () => setInputCode("UP"),
-			[controlMap.down]: () => setInputCode("DOWN"),
-			[controlMap.left]: () => setInputCode("LEFT"),
-			[controlMap.right]: () => setInputCode("RIGHT"),
+			[controlMap.up]: () => cycleStratagems("UP"),
+			[controlMap.down]: () => cycleStratagems("DOWN"),
+			[controlMap.left]: () => cycleStratagems("LEFT"),
+			[controlMap.right]: () => cycleStratagems("RIGHT"),
 		});
 
 		return () => {
 			unsubscribe();
 		};
-	}, []);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [inputCodeIndex, stratagems]);
 
 	return (
 		<div className="container mx-auto flex h-full w-full flex-col items-center gap-4 overflow-hidden py-6 sm:gap-12">
 			<Header />
-			<GameArea inputCode={inputCode} />
+			<GameArea stratagems={stratagems} />
 		</div>
 	);
 }
--- src/components/GameArea.jsx
+++ src/components/GameArea.jsx
@@ -1,18 +1,38 @@
-import { useEffect } from "react";
 import PropTypes from "prop-types";
+import { Icon } from "@iconify/react";
 
-function GameArea({ inputCode }) {
-	useEffect(() => {
-		if (!inputCode) return;
+const iconID = {
+	UP: "fa:arrow-up",
+	DOWN: "fa:arrow-down",
+	LEFT: "fa:arrow-left",
+	RIGHT: "fa:arrow-right",
+};
 
-		console.log(inputCode);
-	}, [inputCode]);
+function GameArea({ stratagems }) {
+	return (
+		<main className="relative flex w-full flex-grow flex-col items-center gap-8 overflow-hidden p-2 lg:w-9/12">
+			<div className="absolute left-0 top-0 z-10 h-full w-full bg-gradient-to-t from-stone-950 from-0% via-transparent"></div>
 
-	return <div>hello world</div>;
+			{stratagems.map(({ name, inputCode }, index) => (
+				<div key={index} className="flex w-full flex-col gap-2">
+					<h1 className="text-sm font-bold uppercase sm:text-xl">{name}</h1>
+					<div className="flex flex-wrap gap-4 rounded-md bg-stone-800 p-4 text-xl sm:text-4xl md:text-3xl">
+						{inputCode.map(({ id, correct }, index) => (
+							<Icon
+								key={index}
+								className={correct && "opacity-50"}
+								icon={iconID[id]}
+							/>
+						))}
+					</div>
+				</div>
+			))}
+		</main>
+	);
 }
 
 GameArea.propTypes = {
-	inputCode: PropTypes.string.isRequired,
+	stratagems: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default GameArea;
